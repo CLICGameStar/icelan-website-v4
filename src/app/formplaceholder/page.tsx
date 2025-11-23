76d2c731e4af3583461126a31f259bea0ad0d3dc
Lorenzo <lorenzo.padrini@epfl.ch>
2025-11-23 02:16:15 +0100

    feat: added form placeholder page

---
+++ src/app/formplaceholder/page.tsx
@@ -0,0 +1,10 @@
+export default async function Form() {
+  return (
+    <>
+      <h2>Les inscriptions ne sont pas encore ouvertes, reviens plus tard!</h2>
+      <p className="description">
+        Retourner à l'<a href="/index.html">accueil</a>
+      </p>
+    </>
+  );
+}
