69f6ff3df06cc11c4a3890d2e1becbd2079e0223
Lorenzo <lorenzo.padrini@epfl.ch>
2025-11-23 02:11:30 +0100

    fix: corrected all remaining small errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,12 @@ export default function Header() {
   const router = useRouter();
   const pathname = usePathname();
   let navLinks = [
-    { name: "Acceuil", href: "/" },
-    { name: "Infos", href: "/infos" },
-    { name: "Règlement", href: "/regles" },
-    { name: "Galerie", href: "/galerie" },
-    { name: "Inscriptions", href: "insc_form_link" },
-    { name: "Staffing", href: "staff_form_link" },
+    { name: "Acceuil", href: "/", color: "highlight-white" },
+    { name: "Infos", href: "/infos", color: "highlight-green" },
+    { name: "Règlement", href: "/regles", color: "highlight-blue" },
+    { name: "Galerie", href: "/galerie", color: "highlight-white" },
+    { name: "Inscriptions", href: "insc_form_link", color: "highlight-green" },
+    { name: "Staffing", href: "staff_form_link", color: "highlight-blue" },
   ];
   return (
     <>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,28 +4,39 @@ import Image from "next/image";
 export default function NavBar({
   navLinks,
 }: {
-  navLinks: { name: string; href: string }[];
+  navLinks: { name: string; href: string; color: string }[];
 }) {
+  let changeNav = () => {
+    let nav = document.getElementById("main-nav");
+    nav?.classList.toggle("nav-active");
+    let burger = document.getElementById("burger-menu");
+    burger?.classList.toggle("burger-highlight");
+  };
   return (
-    <header className="nav-bar">
-      <a href="/" className="logo-link">
+    <header>
+      <a href="/">
         <Image
           src="/logo-white.svg"
+          alt="Icelan logo"
           className="logo"
           width={100}
           height={50}
-          alt="Logo"
         />
       </a>
-      <nav className="nav-bar-right">
+      <nav id="main-nav" className="main-nav">
         <ul>
           {navLinks.map((link) => (
             <li key={link.href}>
-              <Link href={link.href}>{link.name}</Link>
+              <Link href={link.href} className={link.color}>
+                {link.name}{" "}
+              </Link>
             </li>
           ))}
         </ul>
       </nav>
+      <a className="burger-menu" id="burger-menu" href="#" onClick={changeNav}>
+        <img src="/icons/burger-menu.svg" className="icon" alt="Burger menu" />
+      </a>
     </header>
   );
 }
